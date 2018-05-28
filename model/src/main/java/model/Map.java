b85c1aba7827071e3e1578adb56cb602cade5378
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-05-28 16:20:52 +0200

    map & maptester

--- model/src/main/java/model/Map.java
+++ model/src/main/java/model/Map.java
@@ -2,13 +2,19 @@ package model;
 
 public class Map {
 	public Map(){
-		
+		int sizeX;
+		int sizeY;
 	}
 	
 	void mapGenerator(){
 		int x = 0;
 		int y = 0;
+		char entityAtPoint[][] = null;
+		entityAtPoint[0][0] = 'O';
 		
 		
+		if(entityAtPoint[0][0] == 'O'){
+			//Affiche obstacle
+		}
 	}
 }
