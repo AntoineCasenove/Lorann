d4ca1e94eb24a749dc67afa227f4377928e6b808
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-05-28 13:15:40 +0200

    Changement des commentaires

--- contract/src/main/java/model/Example.java
+++ contract/src/main/java/model/Example.java
@@ -1,57 +1,32 @@
 package model;
 
-/**
- * <h1>The Class Example.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
+//The Class Example
 public class Example {
 
-    /** The id. */
+    //The id
     private final int id;
 
-    /** The name. */
+    //The name
     private String    name;
 
-    /**
-     * Instantiates a new example.
-     *
-     * @param id
-     *            the id
-     * @param name
-     *            the name
-     */
+    //Instantiates a new example.
     public Example(final int id, final String name) {
         super();
         this.id = id;
         this.name = name;
     }
 
-    /**
-     * Gets the id.
-     *
-     * @return the id
-     */
+    //Gets the id
     public int getId() {
         return this.id;
     }
 
-    /**
-     * Gets the name.
-     *
-     * @return the name
-     */
+    //Gets the name
     public String getName() {
         return this.name;
     }
 
-    /**
-     * Sets the name.
-     *
-     * @param name
-     *            the new name
-     */
+    //Sets the name
     public void setName(final String name) {
         this.name = name;
     }
