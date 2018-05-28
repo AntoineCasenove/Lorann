73eb3498c4e99a5439cd25b9d9279a632df10201
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-05-28 13:12:57 +0200

    Commentaires dans la classe ControllerFacade.java & package-info.java

--- controller/src/main/java/controller/ControllerFacade.java
+++ controller/src/main/java/controller/ControllerFacade.java
@@ -7,40 +7,23 @@ import model.Example;
 import model.IModel;
 import view.IView;
 
-/**
- * <h1>The Class ControllerFacade provides a facade of the Controller component.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
+//The Class ControllerFacade provides a facade of the Controller component.
 public class ControllerFacade implements IController {
 
-    /** The view. */
+    //The view
     private final IView  view;
 
-    /** The model. */
+    //The model
     private final IModel model;
 
-    /**
-     * Instantiates a new controller facade.
-     *
-     * @param view
-     *            the view
-     * @param model
-     *            the model
-     */
+    //Instantiates a new controller facade.
     public ControllerFacade(final IView view, final IModel model) {
         super();
         this.view = view;
         this.model = model;
     }
 
-    /**
-     * Start.
-     *
-     * @throws SQLException
-     *             the SQL exception
-     */
+    //Start
     public void start() throws SQLException {
         this.getView().displayMessage(this.getModel().getExampleById(1).toString());
 
@@ -55,20 +38,12 @@ public class ControllerFacade implements IController {
         this.getView().displayMessage(message.toString());
     }
 
-    /**
-     * Gets the view.
-     *
-     * @return the view
-     */
+    //Gets the view.
     public IView getView() {
         return this.view;
     }
 
-    /**
-     * Gets the model.
-     *
-     * @return the model
-     */
+    //Gets the model.
     public IModel getModel() {
         return this.model;
     }
