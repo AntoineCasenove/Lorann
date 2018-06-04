1b8e0ce9e23c1846326236c22464abd82efd1440
antoi <antoi@DESKTOP-IKCFU6O>
2018-06-04 22:24:57 +0200

    Javadoc for all (normally)

--- contract/src/main/java/view/Thread_Spell.java
+++ contract/src/main/java/view/Thread_Spell.java
@@ -14,8 +14,7 @@ public class Thread_Spell extends Thread {
 	
 	char direction;
 	
-	private final int loop = 75
-			;
+	private final int loop = 75;
 	
 	public Thread_Spell (IController controller, char direction){
 		this.controller = controller;
